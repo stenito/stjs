b95dce736c5792f209fcf8e37c51f042604adf93
stenito <13723043+stenito@users.noreply.github.com>
2020-07-14 17:20:10 +0200

    random

---
+++ stjs.js
@@ -0,0 +1,11 @@
+// functions for random numbers
+
+const getRandom = () => Math.random();
+
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+
+const getRandomBetween = (min, max) => Math.random() * (max - min) + min;
+
+const getRandomIntBetween = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+console.log(getRandom);
