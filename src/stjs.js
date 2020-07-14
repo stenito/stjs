9a4ef6495b0b17ce133dd8059165e0a6a81eb4bd
stenito <13723043+stenito@users.noreply.github.com>
2020-07-14 18:01:38 +0200

    random

--- src/stjs.js
+++ src/stjs.js
@@ -1,9 +1,13 @@
 // functions for random numbers
 
+// random number between 0 (included) and 1 (not included)
 const getRandom = () => Math.random();
 
+// random integer between 0 (included) and max (not included)
 const getRandomInt = (max) => Math.floor(Math.random() * max);
 
+// random number between min (inluded) and max (not included)
 const getRandomBetween = (min, max) => Math.random() * (max - min) + min;
 
+// random integer between min (included) and max (not included)
 const getRandomIntBetween = (min, max) => Math.floor(Math.random() * (max - min)) + min;
